1198fc66f674c62438609b438f824145afa1ae12
Silvus <hugo.combe.84@gmail.com>
2024-11-23 20:33:55 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 2222, // Port fixe
+    strictPort: true, // Évite que Vite cherche un autre port si 2222 est occupé
+    host: true, // Écoute sur toutes les interfaces
+  }
 })
