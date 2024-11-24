1e3fbb023d4d8fb14e8893f9a0dfbaf4ffbb618b
Silvus <hugo.combe.84@gmail.com>
2024-11-24 21:19:10 +0100

    update ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,12 +2,9 @@ module.exports = {
   apps: [
     {
       name: 'CSSeducteur', // Nom de l'application
-      script: './server.js', // Point d'entrée SSR
+      script: 'npm run dev', // Point d'entrée SSR
       instances: '1', // Utilise tous les cœurs disponibles
       exec_mode: 'cluster',
-      env: {
-        NODE_ENV: 'production',
-      },
     },
   ],
 };
