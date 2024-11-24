f8231d7927ac5b6202919be5b4b9f70f00778205
Silvus <hugo.combe.84@gmail.com>
2024-11-24 14:45:44 +0100

    update vite config 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import fs from 'fs';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Permet d'accéder depuis le proxy
-    port: 2222, // Forcer le port
-    strictPort: true,
-    https: false, // Désactiver HTTPS en dev
-  }
-})
+    host: '0.0.0.0', // Permet d'accéder depuis le réseau
+    port: 2222, // Utilise un port fixe
+    strictPort: true, // Ne change pas automatiquement le port si occupé
+    https: {
+      key: fs.readFileSync('/etc/letsencrypt/live/csseducteur.love/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/csseducteur.love/fullchain.pem'),
+    },
+  },
+});
