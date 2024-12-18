164ad1d0af3ff2bd1b6dad889d5bc695109a2f01
Silvus <hugo.combe.84@gmail.com>
2024-12-18 20:41:34 +0100

    Home page

--- src/Page/Blog.tsx
+++ src/Page/Blog.tsx
@@ -3,6 +3,7 @@ import {getBlog} from "../Tools/Interaction.ts";
 import '../blog.css'
 import IncludeHTML from "../Components/IncludeHTML.tsx";
 import MetaHead from "../Components/MetaHead.tsx";
+import {useEffect, useState} from "react";
 
 export default function Blog() {
   const location = useLocation();
@@ -11,9 +12,15 @@ export default function Blog() {
   const blog = getBlog(slug);
   if (blog === undefined) {
     navigate("/blogs");
-
   }
   const uploadDate = new Date(blog!.uploadDate + 'T00:00:00Z');
+  const [referrer, setReferrer] = useState<string>("");
+
+  useEffect(() => {
+    // Récupère le referer depuis document.referrer
+    setReferrer(document.referrer);
+    console.log("Referer : " + referrer);
+  }, [referrer]);
   return (
     <div id={"blog-page"}
          className={"bg-white rounded-3xl w-9/12 min-h-full self-center m-auto my-5 flex flex-col relative"}>
@@ -25,7 +32,18 @@ export default function Blog() {
         metaImage={blog!.imageName}
         blog={true}
       />
-      <a href="/blog" className={"absolute z-10 m-8 text-base bg-white rounded-3xl py-2 px-4"}>← retour</a>
+      <button
+        onClick={() => {
+          if(referrer.includes("/blog")) {
+            navigate(-1);
+          }else{
+            navigate("/");
+          }
+        }}
+        className={"absolute z-10 m-8 text-base bg-white rounded-3xl py-2 px-4"}
+      >
+        ← retour
+      </button>
       <img
         className={"w-full h-96 rounded-t-3xl object-cover object-top"}
         src={'/blog/Pictures/' + blog!.imageName + '/banner.webp'} alt={blog!.title}
